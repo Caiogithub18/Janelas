056bfabaf0d56d5d385c7ab4e7717e6ea235e690
Caiogithub18 <caioqueirozsouza4@gmail.com>
2024-04-25 22:44:21 -0300

    trabalhando com janelas

---
+++ app/src/main/java/janelas/layout/janela2Application.java
@@ -0,0 +1,14 @@
+package janelas.layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class janela2Application extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.janela2_layout);
+    }
+}
